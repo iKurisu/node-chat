92964d0d817a46cf2a016e6eaf294d97035f545f
iKurisu <chris.epic01@gmail.com>
2020-01-01 15:32:46 +0100

    rename client to pool

--- src/auth.js
+++ src/auth.js
@@ -5,13 +5,13 @@ const craftMsg = field => `${field} should be at least 6 characters long.`;
 const cantSignUp = (action, res) => action === "Sign up" && res.rowCount !== 0;
 const cantSignIn = (action, res) => action === "Sign in" && res.rowCount === 0;
 
-const validateUsername = client => async (username, { action }) => {
+const validateUsername = pool => async (username, { action }) => {
   if (username.length < 6) {
     return craftMsg("Username");
   }
 
   const query = `SELECT * FROM users WHERE username = '${username}'`;
-  const res = await client.query(query);
+  const res = await pool.query(query);
 
   return cantSignUp(action, res)
     ? "Username already exists."
@@ -20,13 +20,13 @@ const validateUsername = client => async (username, { action }) => {
     : true;
 };
 
-const validatePassword = client => async (password, { action, username }) => {
+const validatePassword = pool => async (password, { action, username }) => {
   if (password.length < 6) {
     return craftMsg("Password");
   }
 
   const query = `SELECT * FROM users WHERE username = '${username}'`;
-  const res = await client.query(query);
+  const res = await pool.query(query);
 
   const matches =
     action === "Sign in" && res.rowCount !== 0
@@ -36,10 +36,10 @@ const validatePassword = client => async (password, { action, username }) => {
   return !matches ? "Password is incorrect." : true;
 };
 
-const signUp = client => async ({ username, password }) => {
+const signUp = pool => async ({ username, password }) => {
   const hashedPassword = await bcrypt.hash(password, 8);
 
-  await client.query(
+  await pool.query(
     `INSERT INTO users VALUES('${username}', '${hashedPassword}')`
   );
 };
--- src/server.js
+++ src/server.js
@@ -4,22 +4,22 @@ const { Pool } = require("pg");
 const { validateUsername, validatePassword, signUp } = require("./auth");
 const { VALIDATE_USERNAME, VALIDATE_PASSWORD, SIGN_UP } = require("./events");
 
-const client = new Pool({
+const pool = new Pool({
   connectionString: process.env.TEST_DB_CONNECTION_STRING,
   ssl: true
 });
 
-client.connect();
+pool.connect();
 
 io.on("connection", socket => {
   socket.on(VALIDATE_USERNAME, async ({ username, action }) => {
-    const response = await validateUsername(client)(username, { action });
+    const response = await validateUsername(pool)(username, { action });
 
     socket.emit(VALIDATE_USERNAME, response);
   });
 
   socket.on(VALIDATE_PASSWORD, async ({ action, username, password }) => {
-    const response = await validatePassword(client)(password, {
+    const response = await validatePassword(pool)(password, {
       action,
       username
     });
@@ -28,7 +28,7 @@ io.on("connection", socket => {
   });
 
   socket.on(SIGN_UP, async ({ username, password }) => {
-    await signUp(client)({ username, password });
+    await signUp(pool)({ username, password });
   });
 });
 
